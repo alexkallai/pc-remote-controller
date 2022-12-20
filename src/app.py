ff50264da8f5ca4e88a863bf388b99acadb30882
alexkallai <scythe1092@gmail.com>
2022-12-20 11:49:36 +0100

    Added config support

--- src/app.py
+++ src/app.py
@@ -1,4 +1,9 @@
-from flask import Flask, redirect, url_for, render_template, request, make_response
+from flask import Flask, render_template, request, make_response
+import configparser
+
+
+config = configparser.ConfigParser()
+config.read("settings.cfg")
 
 app = Flask(__name__)
 
@@ -8,13 +13,15 @@ def home():
     if request.method == "GET":
         return render_template("index.html")
 
+# Separate endpoint for the API
 @app.route("/api", methods=["POST"])
 def api_handler():
     if request.method == "POST":
-        print("POST works")
         print(request.get_json())
     # TODO: understand why return is necessary
     return make_response()
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    HOST = config["general"]["HOST"]
+    PORT = config["general"]["PORT"]
+    app.run(host=HOST, port=PORT, debug=True)
