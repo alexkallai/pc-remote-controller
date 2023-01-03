71377727464bc9a973d112cb10083d1cdebfc4eb
alexkallai <scythe1092@gmail.com>
2023-01-03 20:29:05 +0100

    added config copy line into release generating script

--- _generate_release.cmd
+++ _generate_release.cmd
@@ -1,3 +1,5 @@
 CALL .\venv\Scripts\activate.bat
 cd src
 py -m PyInstaller --uac-admin --distpath ".\..\_WINDOWS_RELEASE" --workpath ".\..\_WINDOWS_BUILD" -w -F --add-data "templates;templates" --add-data "static;static" --add-data "remote_icon.png;." app.py
+cd ..
+copy .\settings.cfg .\_WINDOWS_RELEASE\
